b08aded1e4a390d8c67912fd2c165d0cc7308577
P37RIFIED <105243735+P37RIFIED@users.noreply.github.com>
2023-06-06 09:00:51 +0545

    adas

--- test.js
+++ test.js
@@ -1,4 +1,22 @@
-function number(a,b){
-    typeof(a);
-    "string";
+class Train {
+    constructor(color, lightsOn) {
+        this.color = color;
+        this.lightsOn = lightsOn;
+    }
+    toggleLights() {
+        this.lightsOn = !this.lightsOn;
+    }
+    lightsStatus() {
+        console.log('Lights on?', this.lightsOn);
+    }
+    getSelf() {
+        console.log(this);
+    }
+    getPrototype() {
+        var proto = Object.getPrototypeOf(this);
+        console.log(proto);
+    }
 }
+
+myTrain = new Train("black", false);
+console.log(myTrain.lightsStatus());
